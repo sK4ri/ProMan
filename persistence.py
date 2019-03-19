84f5cfe29bad352e14e4e8c744f18a4102b4f32b
Patrick <bakospatrick@gmail.com>
2019-03-19 12:49:37 +0100

    database connection

--- persistence.py
+++ persistence.py
@@ -1,51 +1,82 @@
-import csv
-
-STATUSES_FILE = './data/statuses.csv'
-BOARDS_FILE = './data/boards.csv'
-CARDS_FILE = './data/cards.csv'
-
-_cache = {}  # We store cached data in this dict to avoid multiple file readings
+import psycopg2
+import psycopg2.extras
 
 
-def _read_csv(file_name):
-    """
-    Reads content of a .csv file
-    :param file_name: relative path to data file
-    :return: OrderedDict
-    """
-    with open(file_name) as boards:
-        rows = csv.DictReader(boards, delimiter=',', quotechar='"')
-        formatted_data = []
-        for row in rows:
-            formatted_data.append(dict(row))
-        return formatted_data
+def get_connection_string():
+    user_name = 'rudipapa'
+    password = 'veralynn'
+    host = 'localhost'
+    database_name = 'proman'
+
+    env_variables_defined = user_name and password and host and database_name
+
+    if env_variables_defined:
+        return 'postgresql://{user_name}:{password}@{host}/{database_name}'.format(
+            user_name=user_name,
+            password=password,
+            host=host,
+            database_name=database_name
+        )
+    else:
+        raise KeyError('Some necessary environment variable(s) are not defined')
 
 
-def _get_data(data_type, file, force):
-    """
-    Reads defined type of data from file or cache
-    :param data_type: key where the data is stored in cache
-    :param file: relative path to data file
-    :param force: if set to True, cache will be ignored
-    :return: OrderedDict
-    """
-    if force or data_type not in _cache:
-        _cache[data_type] = _read_csv(file)
-    return _cache[data_type]
+def open_database():
+    try:
+        connection_string = get_connection_string()
+        connection = psycopg2.connect(connection_string)
+        connection.autocommit = True
+    except psycopg2.DatabaseError as exception:
+        print('Database connection problem')
+        raise exception
+    return connection
+
+
+def connection_handler(function):
+    def wrapper(*args, **kwargs):
+        connection = open_database()
+        dict_cur = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        ret_value = function(dict_cur, *args, **kwargs)
+        dict_cur.close()
+        connection.close()
+        return ret_value
+
+    return wrapper
+
+
+_cache = {}
+
+
+@connection_handler
+def get_statuses(cursor, force=False):
+    if force or 'statuses' not in _cache:
+        cursor.execute('''
+                        SELECT id, title FROM statuses;
+        ''')
+        _cache['statuses'] = cursor.fetchall()
+    return _cache['statuses']
+
+
+@connection_handler
+def get_cards(cursor, force=False):
+    if force or 'cards' not in _cache:
+        cursor.execute('''
+                        SELECT id, board_id, title, status_id, cards.order FROM cards;
+        ''')
+        _cache['boards'] = cursor.fetchall()
+    return _cache['cards']
+
+
+@connection_handler
+def get_boards(cursor, force=False):
+    if force or 'boards' not in _cache:
+        cursor.execute('''
+                        SELECT id, title FROM boards;
+        ''')
+        _cache['boards'] = cursor.fetchall()
+    return _cache['boards']
 
 
 def clear_cache():
     for k in list(_cache.keys()):
         _cache.pop(k)
-
-
-def get_statuses(force=False):
-    return _get_data('statuses', STATUSES_FILE, force)
-
-
-def get_boards(force=False):
-    return _get_data('boards', BOARDS_FILE, force)
-
-
-def get_cards(force=False):
-    return _get_data('cards', CARDS_FILE, force)
